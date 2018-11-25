e3c129c444435fa060756c7483d403e412f7189f
Tom <Tom271@users.noreply.github.com>
2018-11-25 19:21:03 +0000

    initial

---
+++ CodeNight01/Tom/FNPlot.m
@@ -0,0 +1,33 @@
+function FNPlot = FNPlot(epsilon,a,b,g,tSpan,initialValue)
+% FNPLOT plots solutions to the FitzHugh-Nagumo model
+% on the interval [0 tSpan] in the x-y plane
+    assert(epsilon>=0 & tSpan>0)
+    
+    [t,x]=ode15s(@(t,x) FN(t,x,epsilon,a,b,g), [0,tSpan], initialValue);
+    
+    fig=figure(1);
+    set(fig,'color','white')    
+
+    [quivx,quivy]=meshgrid(-1.5:0.1:1.5);
+    dquivx=quivx-quivx.^3+quivy;
+    dquivy=epsilon*(a-b*quivx-g*quivy);
+    hold on
+    quiver(quivx,quivy,dquivx,dquivy,'Color','b')
+    plot(x(:,1),x(:,2),'-o','color','r')
+    
+    fold1=[1/sqrt(3),-2/(3*sqrt(3))];
+    fold2=-fold1;
+    equil=[a/b,
+    plot(fold1(1),fold1(2),'g-s','MarkerFaceColor','g','MarkerSize',10)
+    plot(fold2(1),fold2(2),'g-s','MarkerFaceColor','g','MarkerSize',10)
+    plot(equil(1),equil(2),'b-s','MarkerFaceColor','b','MarkerSize',10)
+    
+    nullx=linspace(-1.5,1.5);
+    nully=nullx;
+    plot(nullx,nullx.^3-nullx,'--','LineWidth',1.5,'Color',1/255*[150,150,150]);
+    plot((a-g*nully)/b,nully,'--','LineWidth',1.5,'Color',1/255*[150,150,150]);
+end
+
+function FN=FN(t,x,epsilon,a,b,g)
+    FN=[(x(1)-x(1).^3+x(2)) ;epsilon*(a-b*x(1)-g*x(2))];
+end 
